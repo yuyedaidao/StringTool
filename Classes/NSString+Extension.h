84eaa943af11dc06788ede388a0841535e9450a9
yuyedaidao <wyqpadding@gmail.com>
2014-08-25 11:47:31 +0800

    first

---
+++ Classes/NSString+Extension.h
@@ -0,0 +1,13 @@
+//
+//  NSString+Extension.h
+//  StringTool
+//
+//  Created by Wang on 14-8-25.
+//  Copyright (c) 2014年 Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Extension)
+
+@end
---
+++ Classes/NSString+Extension.m
@@ -0,0 +1,91 @@
+//
+//  NSString+Extension.m
+//  StringTool
+//
+//  Created by Wang on 14-8-25.
+//  Copyright (c) 2014年 Wang. All rights reserved.
+//
+
+#import "NSString+Extension.h"
+
+@implementation NSString (Extension)
+
+//验证email
+-(BOOL)isValidateEmail {
+    
+    NSString *strRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{1,5}";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", strRegex];
+    return [predicate evaluateWithObject:self];
+}
+
+//验证电话号码
+-(BOOL)isValidateTelNumber{
+    
+    NSString *strRegex = @"[0-9]{1,20}";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", strRegex];
+    return [predicate evaluateWithObject:self];
+}
+// 正则判断手机号码地址格式
+- (BOOL)isMobileNumber{
+    
+    //    /**
+    //     * 手机号码
+    //     * 移动：134[0-8],135,136,137,138,139,150,151,157,158,159,182,187,188
+    //     * 联通：130,131,132,152,155,156,185,186
+    //     * 电信：133,1349,153,180,189
+    //     */
+    //    NSString * MOBILE = @"^1(3[0-9]|5[0-35-9]|8[025-9])\\d{8}$";
+    //    /**
+    //     10         * 中国移动：China Mobile
+    //     11         * 134[0-8],135,136,137,138,139,150,151,157,158,159,182,187,188
+    //     12         */
+    //    NSString * CM = @"^1(34[0-8]|(3[5-9]|5[017-9]|8[278])\\d)\\d{7}$";
+    //    /**
+    //     15         * 中国联通：China Unicom
+    //     16         * 130,131,132,152,155,156,185,186
+    //     17         */
+    //    NSString * CU = @"^1(3[0-2]|5[256]|8[56])\\d{8}$";
+    //    /**
+    //     20         * 中国电信：China Telecom
+    //     21         * 133,1349,153,180,189
+    //     22         */
+    //    NSString * CT = @"^1((33|53|8[09])[0-9]|349)\\d{7}$";
+    //    /**
+    //     25         * 大陆地区固话及小灵通
+    //     26         * 区号：010,020,021,022,023,024,025,027,028,029
+    //     27         * 号码：七位或八位
+    //     28         */
+    //    // NSString * PHS = @"^0(10|2[0-5789]|\\d{3})\\d{7,8}$";
+    //
+    //    NSPredicate *regextestmobile = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", MOBILE];
+    //    NSPredicate *regextestcm = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CM];
+    //    NSPredicate *regextestcu = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CU];
+    //    NSPredicate *regextestct = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CT];
+    //
+    //    if (([regextestmobile evaluateWithObject:mobileNum] == YES)
+    //        || ([regextestcm evaluateWithObject:mobileNum] == YES)
+    //        || ([regextestct evaluateWithObject:mobileNum] == YES)
+    //        || ([regextestcu evaluateWithObject:mobileNum] == YES))
+    //    {
+    //        if([regextestcm evaluateWithObject:mobileNum] == YES) {
+    //            NSLog(@"China Mobile");
+    //        } else if([regextestct evaluateWithObject:mobileNum] == YES) {
+    //            NSLog(@"China Telecom");
+    //        } else if ([regextestcu evaluateWithObject:mobileNum] == YES) {
+    //            NSLog(@"China Unicom");
+    //        } else {
+    //            NSLog(@"Unknow");
+    //        }
+    //
+    //        return YES;
+    //    }
+    //    else
+    //    {
+    //        return NO;
+    //    }
+    NSString *strRegex = @"^1([3-8]\\d)\\d{8}$";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", strRegex];
+    return [predicate evaluateWithObject:self];
+}
+    
+@end
